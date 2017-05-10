733b73b09dd0ef430acc5f60ab92b7c2c599b518
Mr-litt <137057181@qq.com>
2017-05-10 17:44:21 +0800

    add comments

--- demo/app/service/SiteService.php
+++ demo/app/service/SiteService.php
@@ -1,14 +1,14 @@
 <?php
 
+/**
+ * Application.php
+ *
+ * @author  Mr.litt<137057181@qq.com>
+ * @date    2017/5/9
+ */
+
 namespace app\service;
 use app\helpful\ErrorCode;
-
-/**
- * Created by IntelliJ IDEA.
- * User: tao
- * Date: 2017/5/9
- * Time: 20:45
- */
 
 /**
  * 类演示文件
@@ -21,8 +21,8 @@ class SiteService
     public function index(){
         $existError = true;
         if($existError){
-            echo ErrorCode::ERROR_DEMO1;
-            echo ErrorCode::ERROR_DEMO1;
+            echo ErrorCode::ERROR_DEMO1;//错误标识1
+            echo ErrorCode::ERROR_DEMO2;//错误标识2
         }
     }
 }
